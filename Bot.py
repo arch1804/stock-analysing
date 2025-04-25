8ff1460e519a45f9c708e349049bf9f5ef6dd9b9
arch1804 <149616149+arch1804@users.noreply.github.com>
2025-04-25 14:53:27 +0530

    Update2 Bot.py

--- Bot.py
+++ Bot.py
@@ -30,3 +30,28 @@ class bot:
       return daylys
 
 
+    def learn():
+        ANN = {}
+        for v in self.symbols :
+            df = pd.read_csv('./analyse/'+v+'.csv')
+            df = df.dropna(axis=0)
+            x = np.matrix(df[["EMA","RSI_14","macd","bbands","PSL_12","ENTP_10","EBSW_40_10", "KURT_30", "candle"]].to_numpy() )
+            y1 = np.matrix(df[["buy"]].to_numpy())
+            y2 = np.matrix(df[["sell"]].to_numpy())
+            #y3 = np.matrix(df[["10_on_pips"]].to_numpy())
+            #y4 = np.matrix(df[["5_on_pips"]].to_numpy())
+            #y5 = np.matrix(df[["pips"]].to_numpy())
+            #y6 = np.matrix(df[["2_pips"]].to_numpy())
+            lrX = np.matrix(df.iloc[:-1 , :][["EMA","RSI_14","macd","bbands","PSL_12","ENTP_10","EBSW_40_10", "KURT_30", "candle"]].to_numpy() )
+            lrY1 =  np.matrix(df.iloc[1: , :][["support"]].to_numpy())
+            lrY2 =  np.matrix(df.iloc[1: , :][["resistance"]].to_numpy())
+            Lr1 = linearregression.linearregression(lrX,lrY1)
+            Beta1, rss1 = Lr1.leastsquare()
+            Lr2 = linearregression.linearregression(lrX,lrY2)
+            Beta2, rss2 = Lr2.leastsquare()
+            ann_sell = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y1)
+            ann_buy = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y2)
+            #ann_10_on_pips = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y3)
+            #ann_5_on_pips = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y4)
+            #ann_pips = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y5)
+            #ann_2_pips = artificialneuralnetwork_classifier.artificialneuralnetwork_classifier(x,y6)
