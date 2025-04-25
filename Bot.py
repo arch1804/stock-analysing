d5b8b6f52c8a4cda837bf35522fd71f2945a07eb
arch1804 <149616149+arch1804@users.noreply.github.com>
2025-04-25 14:52:52 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,6 +10,23 @@ import pandas as pd
 import numpy as np
 import argparse
 
+class bot:
 
+  def __init__(self,type="",start_date=datetime.datetime(2021,1,1),symbol=""):
+
+    def save_data():
+      daylys = []
+      for v in self.symbols :
+        start = self.start_date
+        end = datetime.date.today() #- timedelta(days=1)
+        try:
+            #df = web.DataReader(v, "yahoo", start , end)
+            df = pdr.get_data_yahoo(v, start , end, interval='d')
+            daylys.append(df)
+            df.to_csv('./data/'+v+'.csv')
+        except Exception as e:
+            print(e)
+            pass
+      return daylys
 
 
