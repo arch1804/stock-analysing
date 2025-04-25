859c3df3352a45e1d5fdae2da67dea68be5750a3
arch1804 <149616149+arch1804@users.noreply.github.com>
2025-04-25 14:52:19 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,15 @@
+from lib import market
+import datetime
+from datetime import  timedelta
+import pandas_datareader.data as web
+import pandas_datareader as pdr
+import pandas_ta as ta
+from math4machinelearning.artificialneuralnetwork import artificialneuralnetwork_classifier
+from math4machinelearning.artificialneuralnetwork import linearregression
+import pandas as pd
+import numpy as np
+import argparse
+
+
+
+
